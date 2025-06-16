333e40db867e1ac9a776ce864a341d009da61179
kauboimausu <malagonngasca@gmail.com>
2025-06-16 14:59:45 -0600

    Added a lot of design so I can start working on the JS functionality and have it displayed properly on page

--- script.js
+++ script.js
@@ -5,17 +5,7 @@ function Book(name, author, yearPublished, pageCount){
     this.name = name;
     this.author = author;
     this.yearPublished = yearPublished;
-    this.pageCount = pageCount;
     this.id = crypto.randomUUID();
-}
-
-// This will change the library index when a button is pressed on the page
-// Whether it increases depends on the button pressed
-function changeLibraryIndex(increase) {
-    if(increase) {
-        // This makes sure we reset the index in case of overflow
-        index = index++ % myLibrary.length;
-    }
 }
 
 function addBookToLibrary(name, author, yearPublished, pageCount) {
@@ -25,9 +15,9 @@ function addBookToLibrary(name, author, yearPublished, pageCount) {
 }
 
 function createInitialLibrary() {
-    addBookToLibrary("War and Peace", "Leo Tolstoy", "1867", 1225);
-    addBookToLibrary("East of Eden", "John Steinbeck", 602);
-    addBookToLibrary("Dracula", "Bram Stoker", "1897", 456);
+    addBookToLibrary("War and Peace", "Leo Tolstoy", "1867");
+    addBookToLibrary("East of Eden", "John Steinbeck", "1952");
+    addBookToLibrary("Dracula", "Bram Stoker", "1897");
 }
 
 createInitialLibrary();
